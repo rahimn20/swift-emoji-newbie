1f1e1e8bdec89f9d2f9d3b1b63c4b2f746a69762
Usman Haidar <usman.haidar@ultra.me>
2023-12-09 02:48:26 +0500

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Rahim on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
